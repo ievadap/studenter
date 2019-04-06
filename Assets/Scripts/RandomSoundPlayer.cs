0e913de553709ff2e457d4e072c40070dea525e0
Ieva Dapševičiūtė <ievdps@telesoftas.com>
2019-04-06 22:15:33 +0300

    Initial commit

---
+++ Assets/Scripts/RandomSoundPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSoundPlayer : MonoBehaviour
+{
+    public float AudioInitialDelayInterval;
+    public float AudioRepetitionInterval;
+
+    void Start() {
+        InvokeRepeating("PlaySound", AudioInitialDelayInterval, AudioRepetitionInterval);
+    }
+
+    private void PlaySound() {
+        gameObject.GetComponent<AudioSource>().Play();
+    }
+}
