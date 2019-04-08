6413bb5b9d10c3a1804ee6c20c098630759fc726
Ieva Dapševičiūtė <i.dapseviciute@gmail.com>
2019-04-08 15:00:33 +0300

    Implement Lab2 requirements

---
+++ Assets/Scripts/CollisionParticleEmission.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(ParticleSystem))]
+
+public class TriggeredParticleEmission : MonoBehaviour
+{
+    private ParticleSystem particleSystem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        particleSystem = GetComponent<ParticleSystem>();
+        particleSystem.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            if (particleSystem.isStopped)
+            {
+                particleSystem.Play();
+            }
+            else
+            {
+                particleSystem.Stop();
+
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/RandomDirectionalForce.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomDirectionalForce : MonoBehaviour
+{
+    public float forceRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("ApplyRandomForce", 0, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ApplyRandomForce()
+    {
+        gameObject
+        .GetComponent<Rigidbody>()
+        .AddForce(new Vector3(Random.Range(-forceRange, forceRange), Random.Range(-forceRange, forceRange), Random.Range(-forceRange, forceRange)));
+    }
+}
