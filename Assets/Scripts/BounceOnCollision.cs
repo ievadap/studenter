11e6c4f6ba50946cc0d0fdfbe292b16cf19b49d1
Ieva Dapševičiūtė <i.dapseviciute@gmail.com>
2019-04-15 14:16:18 +0300

    Created BounceOnCollision script for defence task to control object's bounciness depending on collision count

---
+++ Assets/Scripts/BounceOnCollision.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceOnCollision : MonoBehaviour
+{
+    public int maxBounces = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (maxBounces > 1)
+        {
+            maxBounces -= 1;
+        }
+        else
+        {
+            gameObject.GetComponent<BoxCollider>().material.bounciness = 0;
+        }
+    }
+}
